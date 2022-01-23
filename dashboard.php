afc7d1207a5e39750735adad3535e894d41a063e
ChristeenaJose <45913504+ChristeenaJose@users.noreply.github.com>
2022-01-23 02:21:49 +0100

    Setup Dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dashboard - Client area</title>
+    <?php
+    include('module/header.php');
+    ?>
+</head>
+<body>
 <?php
+include('functions.php');
+include('module/auth_session.php');
+?><div class="form">
+    <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
+    <p>You are in user dashboard page.</p>
+    <p><a href="logout.php">Logout</a></p>
+</div><?php
+
+include('module/footer.php');
+?>
+</body>
+</html>
